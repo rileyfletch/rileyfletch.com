b65e34755ae6a8953c3dd13b460a5cbdee7b0696
riley <89424122+nrfletcher@users.noreply.github.com>
2025-11-23 20:01:15 -0600

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Lato } from 'next/font/google'
+import { Poppins } from 'next/font/google'
  
-const roboto = Lato({
+const roboto = Poppins({
   weight: '400',
   subsets: ['latin'],
 })
